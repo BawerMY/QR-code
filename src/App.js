4273128c247c6228e443f5907515c23987686e5c
muhammedbawery <bawermy123@gmail.com>
2023-01-28 21:20:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#d6e2f0] min-h-screen outfit flex justify-center items-center">
+        <div className="rounded-[22px] gap-[25px] text-[30px] text-center p-[20px] flex flex-col w-[400px] max-w-[calc(100vw-80px)] pb-[50px] bg-[#ffffff]">
+            <img className="rounded-[14px]" src="/QR_code_component/images/image-qr-code.png" alt="QR code" />
+            <h1 className="px-[10px] font-bold text-[28px] text-[#1f3251]">Improve your front-end skills by building projects</h1>
+            <p className="px-[10px] text-[20px] text-[#7b879d] font-normal leading-6">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>        
     </div>
   );
 }
+
 
 export default App;
